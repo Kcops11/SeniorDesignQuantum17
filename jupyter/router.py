19798d5c098b81f08bc912c0c271e756d75f534e
bcamick <bcamick@iastate.edu>
2023-10-15 21:42:35 -0500

    got sockets working (with some errors)

--- jupyter/router.py
+++ jupyter/router.py
@@ -1,57 +1,42 @@
 import socket
 import threading
-import json
-
-class CustomPacket:
-    def __init__(self, packet_type, data):
-        self.packet_type = packet_type
-        self.data = data
-
-def serialize_packet(packet):
-    return json.dumps({
-        'packet_type': packet.packet_type,
-        'data': packet.data
-    }).encode('utf-8')
-
-def deserialize_packet(packet_bytes):
-    packet_dict = json.loads(packet_bytes.decode('utf-8'))
-    return CustomPacket(packet_dict['packet_type'], packet_dict['data'])
 
 class Router:
     def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.connections = []
+        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def start(self):
-        self.server_socket.bind((self.host, self.port))
-        self.server_socket.listen(5)  # Adjust the backlog as needed
-        print(f"Router listening on {self.host}:{self.port}")
+    def bind(self):
+        self.serverSocket.bind((self.host, self.port))
+        self.serverSocket.listen(5)
+             
+    def run(self):
+        print ('Waiting for a connection')
+        self.serverSocket.listen(5)
+        (clientSocket, addr) = self.serverSocket.accept()
+        print ('Got a connection from {}'.format(str(addr)))
         while True:
-            self.conn, addr = self.server_socket.accept()
-            print(f"Connection from {addr}")
-            self.connections.append(conn)
-            threading.Thread(target=self.handle_connection, args=(conn,)).start()
+            self.recieveMessage()
+            
+    def sendMessage(self, message, clientSocket):
+        clientSocket.send(message.encode('ascii'))
 
-    def handle_connection(self):
-        while True:
-            packet_bytes = self.conn.recv(1024)  # Adjust buffer size as needed
-            if not packet_bytes:
-                break
-            packet = deserialize_packet(packet_bytes)
-            response_packet = self.process_packet(packet)
-            serialized_response = serialize_packet(response_packet)
-            conn.send(serialized_response)
-            print("Here")
+    def recieveMessage(self):
+        (clientSocket, addr) = self.serverSocket.accept()
+        message = clientSocket.recv(1024).decode('ascii')
+        print(message)
 
-    def process_packet(self, packet):
-        # Implement your packet processing logic here
-        pass
+    def closeSocket(self):
+        self.serverSocket.close()
 
-    def broadcast(self):
-        self.conn.send("Hello World")
+#
+'''
+USE ORDER
+1 make object
+2 bind()
+3 run ()
+4 recieve() 
+5 close()
 
-# Usage example:
-#router = Router('localhost', 8000)
-#router.start()
+'''
--- jupyter/router_testing.py
+++ jupyter/router_testing.py
@@ -1,10 +1,22 @@
 # Main file for testing router.py
+'''
+USE ORDER
+1 make object
+2 bind()
+3 run ()
+4 recieve() 
+5 close()
 
+'''
 from router import Router
-from clustercomputer import QuantumClusterComputer
+from node import nodeComputer
 
-r = Router('', 5001)
+r = Router('127.0.0.1', 5001)
 
-r.start
-r.handle_connection()
-r.broadcast()
+r.bind()
+print("1")
+r.run()
+print("2")
+
+
+print("Router Done")
