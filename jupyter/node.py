e6210b2817f6b0489500ff672edad90fb4b85d95
bcamick <bcamick@iastate.edu>
2023-11-05 12:25:19 -0600

    added comments

--- jupyter/node.py
+++ jupyter/node.py
@@ -2,15 +2,31 @@ import qiskit  # Ensure you have Qiskit installed
 import socket
 import threading
 import time
+'''
+USAGE - this sends a user input to the connected router
 
+
+
+node = nodeComputer('127.0.0.1', 5001)
+node.connect()
+while(True):
+    msg = input("Input message>> ")
+    node.connect()
+    node.sendMessage(msg)
+
+
+
+'''
 class nodeComputer:
     def __init__(self, router_host, router_port, ip):
+        # init a  bunch of vars - done automatically 
         self.router_host = router_host
         self.router_port = router_port
         self.ip = ip
         self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def connect(self):
+        #connects self to router - router should be started first 
         self.serverSocket = socket.socket()
         self.serverSocket.connect((self.router_host, self.router_port))
         
@@ -18,12 +34,14 @@ class nodeComputer:
         t1.start()
 
     def recieveMessage(self):
+        # waits to recieve message - should be placed in a while loop
         message = self.serverSocket.recv(1024).decode('ascii')
         print(message)
         if message == "quit":
             self.closeSocket()
 
     def sendMessage(self, message):
+        # sends message to router 
         self.serverSocket.send(message.encode('ascii'))
         if message == "quit":
             self.closeSocket()
--- jupyter/router.py
+++ jupyter/router.py
@@ -1,18 +1,29 @@
 import socket
 import threading
+'''
+USE ORDER
+1 make object
+2 bind()
+3 run ()
+4 recieve() 
+5 close()
 
+'''
 class Router:
     def __init__(self, host, port):
+        # just sets a bunch of variables - done automatically 
         self.host = host
         self.port = port
         self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def bind(self):
+        # binds self to port 
         self.serverSocket.bind((self.host, self.port))
         self.serverSocket.listen(5)
              
     def run(self):
+        # waits for a connection from the nodes. Should be placed in a while loop 
         print ('Waiting for a connection')
         self.serverSocket.listen(5)
         (self.clientSocket, addr) = self.serverSocket.accept()
@@ -23,9 +34,11 @@ class Router:
         
             
     def sendMessage(self, message):
+        # this sends a message to the node. To use, sendMessage("HELLOWORLD") will send "HELLOWORLD" to node
         self.clientSocket.send(message.encode('ascii'))
 
     def recieveMessage(self):
+        # this waits for a messaged to be recieved from the node
         while(1):
             (self.clientSocket, addr) = self.serverSocket.accept()
             message = self.clientSocket.recv(1024).decode('ascii')
@@ -36,6 +49,7 @@ class Router:
                 t1.join()
 
     def closeSocket(self):
+        # closes port - if you dont do this then it pretty much just runs constantly and is hard to close 
         self.serverSocket.close()
 
 #
