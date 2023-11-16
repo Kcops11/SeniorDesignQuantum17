4ec4923c12e7fc443e3df684cd4573d27fc22bde
Derrick Wright <dwright@iastate.edu>
2023-11-16 15:22:29 -0600

    Some light integration work

--- jupyter/integration_node.py
+++ jupyter/integration_node.py
@@ -3,6 +3,7 @@ import random
 import time
 
 nodes = []
+global num_nodes
 for i in range(1000):
     host = '127.0.0.1'
     ip = '127.' + '.'.join(str(random.randint(1, 9)) for _ in range(3))
@@ -10,11 +11,14 @@ for i in range(1000):
     print("Node " + node.ip + " created")
     nodes.append(node)
 
+num_nodes = 0
 
 for n in nodes:
     n.connect()
-    print("Node", n.router_host, "connected")
+    print("Node", n.ip, "connected")
     n.doWork()
-    print("Node", n.router_host, "on standby")
-    time.sleep(3)
+    print("Node", n.ip, "on standby")
+    num_nodes += 1 
+    print(f"Num nodes: {num_nodes}")
+    time.sleep(10)
 
